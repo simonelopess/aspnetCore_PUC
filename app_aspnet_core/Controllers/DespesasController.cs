ee6075690b5a5e880cc341eaefe6e3e580ee53ca
DESKTOP-LF77V8L\simon <sisilopes20@gmail.com>
2021-04-05 22:32:01 -0300

    Criação dos controllers

---
+++ app_aspnet_core/Controllers/DespesasController.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using app_aspnet_core.Data;
+using app_aspnet_core.Models;
+
+namespace app_aspnet_core.Controllers
+{
+    public class DespesasController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public DespesasController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Despesas
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Despesa.Include(d => d.Categoria);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Despesas/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var despesa = await _context.Despesa
+                .Include(d => d.Categoria)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (despesa == null)
+            {
+                return NotFound();
+            }
+
+            return View(despesa);
+        }
+
+        // GET: Despesas/Create
+        public IActionResult Create()
+        {
+            ViewData["CategoriaId"] = new SelectList(_context.Categoria, "Id", "Descricao");
+            return View();
+        }
+
+        // POST: Despesas/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Descricao,Data,Valor,CategoriaId")] Despesa despesa)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(despesa);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CategoriaId"] = new SelectList(_context.Categoria, "Id", "Descricao", despesa.CategoriaId);
+            return View(despesa);
+        }
+
+        // GET: Despesas/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var despesa = await _context.Despesa.FindAsync(id);
+            if (despesa == null)
+            {
+                return NotFound();
+            }
+            ViewData["CategoriaId"] = new SelectList(_context.Categoria, "Id", "Descricao", despesa.CategoriaId);
+            return View(despesa);
+        }
+
+        // POST: Despesas/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Descricao,Data,Valor,CategoriaId")] Despesa despesa)
+        {
+            if (id != despesa.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(despesa);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!DespesaExists(despesa.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CategoriaId"] = new SelectList(_context.Categoria, "Id", "Descricao", despesa.CategoriaId);
+            return View(despesa);
+        }
+
+        // GET: Despesas/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var despesa = await _context.Despesa
+                .Include(d => d.Categoria)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (despesa == null)
+            {
+                return NotFound();
+            }
+
+            return View(despesa);
+        }
+
+        // POST: Despesas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var despesa = await _context.Despesa.FindAsync(id);
+            _context.Despesa.Remove(despesa);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool DespesaExists(int id)
+        {
+            return _context.Despesa.Any(e => e.Id == id);
+        }
+    }
+}
